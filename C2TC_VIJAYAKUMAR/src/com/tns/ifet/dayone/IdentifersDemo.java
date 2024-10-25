95add860e74042f92dfbdbd24055a825c509eb1d
Peace <Peace@DESKTOP-LEJ7IC6>
2024-10-25 15:06:44 +0530

    data

---
+++ C2TC_VIJAYAKUMAR/src/com/tns/ifet/dayone/IdentifersDemo.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.dayone;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int $number =20;
+     System.out.println("value of the number variable is: "+$number);
+     String studentName="Aniket";
+     System.out.println("value is :"+studentName);
+	}
+
+}
